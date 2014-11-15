c1694ea1e313be2ba6f6a9e8f7f0eb8da2b91967
themetric <marklinstop@gmail.com>
2014-11-14 18:17:27 -0800

    reaction code for GET/POST request and update form

--- src/crbridge/actions/http-request/__init__.py
+++ src/crbridge/actions/http-request/__init__.py
@@ -8,12 +8,12 @@
 import rethinkdb as r
 from rethinkdb.errors import RqlRuntimeError, RqlDriverError
 import syslog
-import time
-import datetime
 import json
+import requests
+import ast
 
 
-def failed(redata, jdata, rdb, r_server):
+def failed(redata, redata, jdata, rdb, r_server):
     ''' This method will be called when a monitor has failed '''
     run = True
     if "failed" in jdata['check']['prev_status']:
@@ -33,11 +33,34 @@ def healthy(redata, jdata, rdb, r_server):
         run = False
 
     if run:
-        result = make_request(jdata, rdb, r_server)
+        result = make_request(jdata, redata, rdb, r_server)
         return result
     else:
         return None
 
 
-def make_request(jdata, rdb, r_server):
+def make_request(jdata, redata, rdb, r_server):
+    ''' Make the HTTP request '''
+    url = redata['url']
+    # Parse the headers from a string to
+    # header like {'Authorization': 'my-token'}
+    if redata['request_headers'] is not None:
+        headers = ast.literal_eval(redata['request_headers'])
+    else:
+        headers = {}
+    timeout = 3.00
+    if redata['request_type'] == "post":
+        body = redata['post_body']
+        try:
+            result = requests.post(
+                url, timeout=timeout, headers=headers, data=body, verify=False)
+        except
+            return False
+    else:
+        try:
+            result = requests.get(
+                url, timeout=timeout, headers=headers, verify=False)
+        except:
+            return False
+    return True
 
